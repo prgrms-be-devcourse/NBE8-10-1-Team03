741d47bc1ebeec74679de22c8e96cd2ac1e14615
unikal1 <jack8226123@gmail.com>
2025-12-15 16:25:00 +0900

    Feat: MySQL 연결 및 jpa config 작성
    - enable auditing 으로 createdDate 등 자동 삽입
    - application.properties 를 통해 mysql 연결 추가
    
    Ref: #1

---
+++ src/main/java/com/nbe8101team03/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.nbe8101team03.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
