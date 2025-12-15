005fb8f0f8c22626c2fa9213ba2123cd2c948c2c
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-12-15 18:15:08 +0900

    Feat: admin entity에 builder 추가
    Ref: #7

--- src/main/java/domain/admin/entity/Admin.java
+++ src/main/java/domain/admin/entity/Admin.java
@@ -1,13 +1,13 @@
 package domain.admin.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 @Table(name="admin")
 public class Admin {
@@ -16,9 +16,24 @@ public class Admin {
     @Column(name = "id")
     private int id;
 
-    @Column(name = "user_id", length = 10, nullable = false)
+    @Column(name = "user_id", length = 10, nullable = false, unique = true)
     private String userId;
 
     @Column(name = "password", length = 255, nullable = false)
     private String password;
+
+    @Builder
+    public Admin(String userId, String password){
+        this.userId = userId;
+        this.password = password;
+    }
+
+    // todo 아이디나 비밀번호 변경에 필요
+//    public void setUserId(String userId) {
+//        this.userId = userId;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
 }
