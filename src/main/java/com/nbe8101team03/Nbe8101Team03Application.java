d695a05907ae5b279e07cab8362141c9b1b9c07a
unikal1 <jack8226123@gmail.com>
2025-12-15 11:11:41 +0900

    init commit

---
+++ src/main/java/com/nbe8101team03/Nbe8101Team03Application.java
@@ -0,0 +1,13 @@
+package com.nbe8101team03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe8101Team03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nbe8101Team03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/nbe8101team03/Nbe8101Team03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbe8101team03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe8101Team03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
