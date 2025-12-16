50b5d4c0bf1cb132d424487a9bed7394ac7f689b
lonelylonelylonely <lonelylonelylonely@example.com>
2025-12-16 09:33:14 +0900

    Feat: 유저 관리 기본 구조 설정
    - controller,dto,service, repository 패키지 및 파일 생성
    Ref:#15

--- src/main/java/domain/member/controller/MemberController.java
+++ src/main/java/domain/member/controller/MemberController.java
@@ -1,4 +1,14 @@
 package domain.member.controller;
 
+import domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+
 }
---
+++ src/main/java/domain/member/dto/MemberDto.java
@@ -0,0 +1,4 @@
+package domain.member.dto;
+
+public class MemberDto {
+}
