d749cccab730cbcbe77cfc672b3efa12fa3fcffd
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-12-19 10:25:22 +0900

    Fix: app 실행 가능한 상태로 변경 Ref: #31

--- src/main/java/com/nbe8101team03/domain/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nbe8101team03.domain.user.repository;
-
-import com.nbe8101team03.domain.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
